bc981ddfac781031ad3a4089d0c12e9d03b626ce
lipeehmartins <lipihmartins@gmail.com>
2021-04-03 23:08:19 -0400

    Add Rest API

---
+++ Seu_Frete_API/Seu_Frete/Seu_Frete/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Seu_Frete project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Seu_Frete.settings')
+
+application = get_wsgi_application()
---
+++ Seu_Frete_API/Seu_Frete/api_SeuFrete/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApiSeufreteConfig(AppConfig):
+    name = 'api_SeuFrete'
